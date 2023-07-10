c7d94f5dc58bf285bc1a255edd2778e2d92f066c
Filippo Nicolini <filippo.nicolini6@unibo.it>
2023-07-10 11:13:59 +0200

    Update main

--- compilation_boxplot/generate_plots.R
+++ compilation_boxplot/generate_plots.R
@@ -1,61 +1,122 @@
+library(dplyr)
+library(viridis)
 library(phangorn)
-library(tidyverse)
+library(ggpubr)
+
+##################################################################
+### GENERATE THE PLOT WITH PRESENCE/ABSENCE DATA ON DMRT GENES ###
+##################################################################
+
+# import and format data
+compilation.raw <- read.table("dmrt.compilation.tsv", header = TRUE, check.names = FALSE)
+colnames(compilation.raw)[5] <- "Dmrt4/5"
+
+compilation.tibble <- dplyr::as_tibble(compilation.raw) %>%
+  tidyr::pivot_longer(!Species, names_to = "Gene", values_to = "Count")
+
+compilation.tibble$Gene["Dmrt45"]
+
+# Species_order <- factor(compilation.tibble$Species, levels = c("Mmar","Mner","Pstr","Scon","Dpol","Dros","Amar","Csin","Mmer","Rphi","Lfor","Bpla","Mphi","Pvir","Mcor","Mgal","Medu","Sbro","Pyes","Pmax","Apur","Airi","Airc","Pmar","Pfum","Pfuc","Sglo","Cvir","Cari","Cgig","Chon"))
+#species ored 4evolution
+Species_order <- factor(compilation.tibble$Species, levels = c("Mmar","Pstr","Scon","Dros","Dpol","Amar","Csin","Rphi","Mmer","Sbro","Pyes","Pmax","Apur","Airi","Airc","Pmar","Sglo","Cvir","Cari","Cgig","Pvir","Mcor","Mgal","Medu"))
+gene_order <- factor(compilation.tibble$Gene, levels = c("Dmrt2","Dmrt3","Dmrt4/5","Dmrt1L"))
+                     
+plot.compilation <- ggplot(compilation.tibble, aes(x = Species_order,
+                                                   y = fct_rev(gene_order),
+                                                   fill = factor(Count))) +
+  geom_tile(color = "grey20", size = 0.4) +
+  geom_text(aes(label = Count), size = 2) +
+  scale_fill_manual(values = c("white", "#D6DBE2", "#9CA8BA", "#58677E")) +
+  scale_x_discrete(position = "top") +
+  theme_bw() +
+  labs(fill = "Count") +
+  theme(panel.grid = element_blank(),
+        axis.text.x = element_text(size = 6, angle = 90, hjust = 0),
+        axis.text.y = element_text(face = 'bold.italic',
+                                   colour = c("#0092ff", "#bbc5d0","#bbc5d0","#bbc5d0")),
+        panel.border = element_rect(size = 1),
+        # axis.text.y = element_text(size = 5),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        legend.position = "none") +
+  coord_equal(expand = 0)
+
+plot.compilation
 
 
-#read amino acid alignments
-dmrt1L.alignment <- read.phyDat("dmrt1L.bivalve.aligned.trim04.faa", format = "fasta", type = "AA")
-dmrt2.alignment <- read.phyDat("dmrt2.bivalve.aligned.trim04.faa", format = "fasta", type = "AA")
-dmrt3.alignment <- read.phyDat("dmrt3.bivalve.aligned.trim04.faa", format = "fasta", type = "AA")
-dmrt45.alignment <- read.phyDat("dmrt45.bivalve.aligned.trim04.faa", format = "fasta", type = "AA")
+#boxplot
+Dmrt1L.alignment <- read.phyDat("Dmrt1L.aligned.trim06.faa", format = "fasta", type = "AA")
+Dmrt1L.dist <- dist.ml(Dmrt1L.alignment, model = "JTT")
+Dmrt1L.dist.list <- Dmrt1L.dist[1:length(Dmrt1L.dist)]
 
-#compute pairwise amino acid distances
-dmrt1L.dist <- dist.ml(dmrt1L.alignment, model = "JTT")
-dmrt2.dist <- dist.ml(dmrt2.alignment, model = "JTT")
-dmrt3.dist <- dist.ml(dmrt3.alignment, model = "JTT")
-dmrt45.dist <- dist.ml(dmrt45.alignment, model = "JTT")
+Dmrt2.alignment <- read.phyDat("Dmrt2.aligned.trim06.faa", format = "fasta", type = "AA")
+Dmrt2.dist <- dist.ml(Dmrt2.alignment, model = "JTT")
+Dmrt2.dist.list <- Dmrt2.dist[1:length(Dmrt2.dist)]
 
-#get the list of computed pairwise amino acid distance
-dmrt1L.dist.list <- dmrt1L.dist[1:length(dmrt1L.dist)]
-dmrt2.dist.list <- dmrt2.dist[1:length(dmrt2.dist)]
-dmrt3.dist.list <- dmrt3.dist[1:length(dmrt3.dist)]
-dmrt45.dist.list <- dmrt45.dist[1:length(dmrt45.dist)]
+Dmrt3.alignment <- read.phyDat("Dmrt3.aligned.trim06.faa", format = "fasta", type = "AA")
+Dmrt3.dist <- dist.ml(Dmrt3.alignment, model = "JTT")
+Dmrt3.dist.list <- Dmrt3.dist[1:length(Dmrt3.dist)]
 
-#prepare the dataframe for the boxplot and statistical tests
-dist.al.distr <- data.frame(c(rep("DmrtL1",length(dmrt1L.dist.list)),
-                              rep("Other Dmrts",length(dmrt2.dist.list)),
-                              rep("Other Dmrts",length(dmrt3.dist.list)),
-                              rep("Other Dmrts",length(dmrt45.dist.list))),
-                            c(dmrt1L.dist.list,
-                              dmrt2.dist.list,
-                              dmrt3.dist.list,
-                              dmrt45.dist.list))
+Dmrt45.alignment <- read.phyDat("Dmrt45.aligned.trim06.faa", format = "fasta", type = "AA")
+Dmrt45.dist <- dist.ml(Dmrt45.alignment, model = "JTT")
+Dmrt45.dist.list <- Dmrt45.dist[1:length(Dmrt45.dist)]
 
-colnames(dist.al.distr) <- c("V1","V2")
+dist.al.distr <- data.frame(c(rep("Dmrt2",length(Dmrt2.dist.list)),
+                              rep("Dmrt3",length(Dmrt3.dist.list)),
+                              rep("Dmrt4/5",length(Dmrt45.dist.list)),
+                              rep("Dmrt1L",length(Dmrt1L.dist.list))),
+                            c(Dmrt2.dist.list,
+                              Dmrt3.dist.list,
+                              Dmrt45.dist.list,
+                              Dmrt1L.dist.list))
 
-#test for normality
-dist.al.distr.lm <- lm(V2 ~ V1, data = dist.al.distr)
+colnames(dist.al.distr) <- c("Gene","Dist")
+
+dist.al.distr$Gene <- factor(dist.al.distr$Gene, levels = c("Dmrt2", "Dmrt3", "Dmrt4/5", "Dmrt1L"))
+
+#statistics related to boxplots
+dist.al.distr.lm <- lm(Dist ~ Gene, data = dist.al.distr)
 qqnorm(resid(dist.al.distr.lm))
-shapiro.test(resid(dist.al.distr.lm))
+shapiro.test(resid(dist.al.distr.lm)) #not normally-distributed
 
-#KW test
-kruskal.test(dist.al.distr$V2, dist.al.distr$V1)
+kruskal.test(dist.al.distr$Dist, dist.al.distr$Gene) #there are significative differences
 
-#Wilcoxon test
-wilcox.test(dist.al.distr$V2~dist.al.distr$V1)
+pairwise.wilcox.test(dist.al.distr$Dist, dist.al.distr$Gene, p.adjust.method = "bonferroni")
 
-#generate the boxplot
-dist.al.plot <- ggplot(dist.al.distr, aes(as.factor(V1), V2, fill = as.factor(V1), color = as.factor(V1))) +
-  geom_boxplot(lwd = 1.3) +
-  ylim(0, 3.33) +
-  geom_segment(aes(x = "DmrtL1", xend = "Other Dmrts", y = 3.26, yend = 3.26), lwd = 1, col = "#7f878f") +
-  annotate("text", label = "****", x = 1.5, y = 3.33, col = "#7f878f", size = 6) +
-  scale_fill_manual(values = alpha(c("#3174bc","#bbc5d0"), 0.5)) +
-  scale_color_manual(values = c("#3174bc","#bbc5d0")) +
+
+boxplot <- ggplot(dist.al.distr, aes(Gene, Dist,
+                                     fill = as.factor(Gene), color = as.factor(Gene))) +
+  geom_boxplot(lwd = 1) +
+  # ylim(0, 3.5) +
+  # geom_segment(aes(x = "Dmrt1L", xend = "Dmrt2", y = 2.8, yend = 2.8), lwd = 0.7, col = "#7f878f") +
+  # geom_segment(aes(x = "Dmrt1L", xend = "Dmrt3", y = 3, yend = 3), lwd = 0.7, col = "#7f878f") +
+  # geom_segment(aes(x = "Dmrt1L", xend = "Dmrt4/5", y = 3.2, yend = 3.2), lwd = 0.7, col = "#7f878f") +
+  # geom_segment(aes(x = "Dmrt3", xend = "Dmrt4/5", y = 2.6, yend = 2.6), lwd = 0.7, col = "#7f878f") +
+  # geom_segment(aes(x = "Dmrt2", xend = "Dmrt4/5", y = 2.4, yend = 2.4), lwd = 0.7, col = "#7f878f") +
+  # annotate("text", label = "****", x = 2.5, y = 2.85, col = "#7f878f", size = 4) +
+  # annotate("text", label = "****", x = 3, y = 3.05, col = "#7f878f", size = 4) +
+  # annotate("text", label = "****", x = 3.5, y = 3.25, col = "#7f878f", size = 4) +
+  # annotate("text", label = "****", x = 2.5, y = 2.65, col = "#7f878f", size = 4) +
+  # annotate("text", label = "***", x = 2, y = 2.45, col = "#7f878f", size = 4) +
+  scale_fill_manual(values = alpha(c("#bbc5d0","#bbc5d0","#bbc5d0","#0092ff"), 0.5)) +
+  scale_color_manual(values = c("#bbc5d0","#bbc5d0","#bbc5d0","#0092ff")) +
   ylab("Amino acid distance") +
   xlab("") +
   theme_light() +
   theme(axis.title.y = element_text(size = 13, vjust = 2.5, color = "#4d4d4d"),
-        axis.text.x = element_text(size = 13, vjust = -0.5),
+        axis.text.x = element_text(size = 13, vjust = -0.5, face = 'bold.italic',
+                                   colour = c("#bbc5d0","#bbc5d0","#bbc5d0","#0092ff")),
         legend.position = "none")
 
-ggsave("dist.al.plot.pdf", dist.al.plot, width = 3.5, height = 6, units = "in", dpi = 400)
+boxplot
+
+
+panel <- ggarrange(plot.compilation, boxplot,
+                   ncol = 1, nrow = 2, heights = c(0.4,1), align = "v",
+                   labels = c("AB","C"))
+
+panel
+
+ggsave("plot.panel.4evolution.pdf", plot = panel, device = "pdf",
+       dpi = 300, height = 6, width = 5.5, units = ("in"))
+
